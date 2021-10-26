014947240d5251e40f80ae56410ad9b4b833ed75
kcamposa <kev_day09@hotmail.es>
2021-10-26 17:53:47 -0600

    v3.6

--- two-traffic-lights.py
+++ two-traffic-lights.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 from gpiozero import Buzzer
 import time
 
+sig = 1
 buzzer = Buzzer(16)
 portList = [23, 24, 25, 13, 19, 26] # green1, yellow1, red1 ------- green2, yellow2, red2
 
@@ -15,10 +16,7 @@ def setup():
 def run():
     try:
         while True:
-            input_state=GPIO.input(6)
-            if input_state == False:
-                ChangingLights()
-            
+            ChangingLights()
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Stop running")
@@ -29,13 +27,16 @@ def ChangingLights():
     GPIO.output(portList, GPIO.LOW)
 
     LEDsON(23,26) # green 1 and red 2 ON
+    CaptureSignal()
     time.sleep(5)
 
     LEDsOFF(23,0) # green 1 OFF
     LEDsON(24,0) # yellow 1 ON
+    CaptureSignal()
     time.sleep(2)
 
     IntermittentLEDs(24,0) # intermittent yellow 1
+    CaptureSignal()
 
     LEDsOFF(26,0) # red 2 OFF
     LEDsON(25,13) # red 1 and green 2 ON
@@ -43,21 +44,19 @@ def ChangingLights():
 
     LEDsOFF(13,0) # green 2 OFF
     LEDsON(19,0) # yellow 2 ON
+    CaptureSignal()
     time.sleep(2)
 
+    buzzer.off()
     IntermittentLEDs(19,0) # intermittent yellow 2
+    CaptureSignal()
 
-def Crosswalk(): 
-    if GPIO.input(6) == GPIO.LOW:
-        GPIO.output(portList, GPIO.LOW)
-        IntermittentLEDs(24,19) # intermittent yellow 1 and 2
-        GPIO.output(portList, GPIO.LOW)
-        #buzzer.beep()
-        LEDsON(25,26) # red 1 and 2 on
-        time.sleep(7)
-        IntermittentLEDs(25,26) # intermittent red 1 and 2
-        #buzzer.off()
-        run()
+def CaptureSignal(): 
+    input_state=GPIO.input(6)
+    if input_state == 0:
+        sig = 0
+        print("cambio de estado")
+        
 
 def LEDsON(led1, led2): # ON led or leds
     if led1 != 0 and led2 !=0:     
@@ -86,10 +85,6 @@ def IntermittentLEDs(led1, led2): # ON intermittent led or leds
             time.sleep(.5)
             LEDsOFF(led1,0)
             time.sleep(.5)
-
-
-def know_state():
-    input_state=GPIO.input(6)
 
 
 setup()
