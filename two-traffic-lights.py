571db0d46ac2591baeff43836591e70bc5fe1d0d
kcamposa <kev_day09@hotmail.es>
2021-10-27 17:29:19 -0600

    v3.8

--- two-traffic-lights.py
+++ two-traffic-lights.py
@@ -3,44 +3,28 @@ import RPi.GPIO as GPIO
 from gpiozero import Buzzer
 import time
 
-sig = False
+sig = 0
 buzzer = Buzzer(16)
-portList = [23, 24, 25, 13, 19, 26] # green1, yellow1, red1 ------- green2, yellow2, red2
+portList = [23, 24, 25, 13, 19, 26, 22] # green1, yellow1, red1 ------- green2, yellow2, red2
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(portList, GPIO.OUT)
+
     GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP) # push button
+    GPIO.add_event_detect(6, GPIO.RISING, callback=CaptureSignal)
 
-
-
-
-# -------- testing this function 
 def run():
     try:
         while True:
-            input_state=GPIO.input(6)
-            if input_state == 1:
-                sig = False
-                #print(sig)
-                ChangingLights(sig)
-                time.sleep(.3)
-            else:
-                sig = True
-                #print(sig)
-                ChangingLights(sig)
-                time.sleep(.1)
-            time.sleep(.2)
+            ChangingLights()
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("Stop running")
     GPIO.cleanup()
-# --------------------------------
 
-
-
-def ChangingLights(signal):
+def ChangingLights():
 
     GPIO.output(portList, GPIO.LOW)
 
@@ -55,23 +39,27 @@ def ChangingLights(signal):
 
     LEDsOFF(26,0) # red 2 OFF
     LEDsON(25,13) # red 1 and green 2 ON
-    #if signal:
-    #    buzzer.beep()
+    activeCrosswalk()
     time.sleep(5)
 
     LEDsOFF(13,0) # green 2 OFF
-    buzzer.beep()
     LEDsON(19,0) # yellow 2 ON
     time.sleep(2)
+    LEDsOFF(22,0) # in case button was pushed
+    buzzer.off() # in case button was pushed
 
-    #buzzer.off()
     IntermittentLEDs(19,0) # intermittent yellow 2
 
-def CaptureSignal(): 
-    input_state=GPIO.input(6)
-    if input_state == 0:
-        sig = 0
-        print("cambio de estado")
+def CaptureSignal(channel):
+    global sig
+    sig = 1
+
+def activeCrosswalk():
+    global sig 
+    if sig == True:
+        LEDsON(22,0)
+        buzzer.beep()
+        sig = False
         
 
 def LEDsON(led1, led2): # ON led or leds
